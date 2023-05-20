e5d92f9f98c2cc47da54218d079e9a572b2022a3
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2023-05-20 20:43:46 +0200

    Add dataset

--- Python-Shiny/app.py
+++ Python-Shiny/app.py
@@ -1,9 +1,9 @@
 from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
-    ui.h2("Hello Shiny!"),
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
+    ui.layout_sidebar(
+        
+    )
 )
 
 
