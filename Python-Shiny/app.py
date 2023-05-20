812ccb5563db3a37909c893b3fb1a694d70ddd16
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2023-05-20 21:18:56 +0200

    Add 2018 dataset

--- Python-Shiny/app.py
+++ Python-Shiny/app.py
@@ -1,17 +1,19 @@
 from shiny import App, render, ui
+import pandas as pd
 
 app_ui = ui.page_fluid(
     ui.layout_sidebar(
-        
-    )
+        ui.panel_sidebar(), 
+        ui.panel_main(ui.output_table("summary")))
 )
 
 
 def server(input, output, session):
     @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 3}"
+    @render.table
+    def summary():
+        df = pd.read_csv("./ddbb/BrazilConflicts2018.csv")
+        return df
 
 
 app = App(app_ui, server)
